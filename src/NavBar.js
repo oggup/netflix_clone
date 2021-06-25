1cf4ebaeaf5e3bd4c0909c62c318fff573772b4a
Oggup <edifoo@gmail.com>
2021-06-25 16:53:57 -0700

    finished the banner and working on nav bar

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="nav">
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
+}
+
+export default NavBar;
